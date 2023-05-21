7156ff0b119af114e19a0428ecf036309f3e0bf2
Triistam <akumaru.senju@gmail.com>
2023-05-21 14:36:10 +0500

    creepy fonts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import Avatar from "@/public/ava.jpg";
+import Head from "next/head";
 
 export default function () {
   return (
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Zen+Tokyo+Zoo&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Eater&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Nosifer&display=swap" rel="stylesheet" /> 
+    </Head>
       <div>
         <div className="avatar mt-12">
         <Image src={Avatar} className="text-center w-72 rounded-full my-3" width={200}
@@ -11,8 +19,8 @@ export default function () {
         alt="Picture of the author" />
       </div>
 
-        <h1 className="text-4xl text-center my-5">Triistam</h1>
-        <p className="text-center text-2xl my-5">Weakling preparing to explode with his unexpected moves.</p>
+        <h1 className="text-4xl text-center my-5 nick">TRIISTAM</h1>
+        <p className="text-center text-2xl my-5 quote">Weakling preparing to explode with his unexpected moves.</p>
       </div>
     </>
   );
