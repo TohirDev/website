915baca4e7530ed7a186d63280a9772510ce870d
Triistam <akumaru.senju@gmail.com>
2023-05-21 19:24:34 +0500

    changed the description meta tag

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Eater&display=swap" rel="stylesheet" /> 
-      <meta name="description" content="Weakling preparing to explode with his unexpected moves." />
+      <meta name="description" content="Weakling preparing to explode with his unexpected moves. Full of sorrows" />
       <meta name="author" content="Triistam" />
       <meta name="keywords" content="Triistam akumaru user6879" />
       <meta name="og:image" content="https://i.imgur.com/nDxSgWi.png"/>
