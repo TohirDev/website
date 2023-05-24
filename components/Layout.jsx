c3dc40d780fbc76d9c8075511068a5b1aa63ecd8
Triistam <akumaru.senju@gmail.com>
2023-05-24 18:49:35 +0500

    remove some meta tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -20,28 +20,9 @@ export default function Layout({ children }) {
       <meta name="og:type" content="profile"/>
       <meta name="og:url" content="https://triistam.vercel.app/"/> */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Nextra" />
-        <meta property="og:description" content="The next site builder" />
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Aqua.svg"
-        />
-        <meta
-          name="og:description"
-          content="O'zbek dasturchilari tomonidan qo'llab quvvatlanadigan Golang hamjamiyati."
-        />
-        <meta
-          name="description"
-          content="O'zbek dasturchilari tomonidan qo'llab quvvatlanadigan Golang hamjamiyati."
-        />
+        <meta name="og:title" content="Triistam's Website" />
+        <meta name="og:description" content="Full of sorrows." />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content="https://go.dev/blog/go-brand/Go-Logo/PNG/Go-Logo_Aqua.png"
-        />
-        <meta name="twitter:site:domain" content="docs-golanguz.vercel.app" />
-        <meta name="twitter:url" content="https://docs-golanguz.vercel.app/" />
         <meta
           name="og:title"
           content="Triistam"
@@ -50,7 +31,6 @@ export default function Layout({ children }) {
           name="og:image"
           content="https://i.imgur.com/lcGaAnf.png"
         />
-        <meta name="apple-mobile-web-app-title" content="Golang Uzbekistan" />
       </Head>
       <div className="container mx-auto" id="app">
         <Navbar />
